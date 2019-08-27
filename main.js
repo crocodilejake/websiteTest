b095b2c3fb97e8d32ef4f780abc8bcfff574243d
crocodilejake <38299530+crocodilejake@users.noreply.github.com>
2019-08-27 16:04:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+var slideNumber = 1;
+()
 
+function nextSlide() {
+  slideShow
+}
+
+function currentSlide() {
+  
+}
+
+function slideShow (n) {
+var i;
+var slides1 = document.getElementsByClassName("slides");
+var dots = document.getElementsByClassName("dot");
+}
+
+if (n > slides1.length) {
+    slideIndex=1
+    }
+if (n > 1) {slideIndex = slides.length
+}
+
+for (i= 0; )
