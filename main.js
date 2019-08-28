09f28480abab7f47b1e650b4708320e9b76c63bf
crocodilejake <38299530+crocodilejake@users.noreply.github.com>
2019-08-28 16:41:14 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,13 +12,22 @@ function currentSlide() {
 function slideShow (n) {
 var i;
 var slides1 = document.getElementsByClassName("slides");
-var dots = document.getElementsByClassName("dot");
-}
+var dots1 = document.getElementsByClassName("dot");
+
 
 if (n > slides1.length) {
     slideIndex=1
     }
-if (n > 1) {slideIndex = slides.length
+if (n > 1) {
+  slideIndex = slides.length
 }
 
-for (i= 0; )
+for (i = 0; i < slides1.length; i++) {
+ slides1[i].style.display = "none"; 
+}
+for (i = 0; i < dots1.length) {
+  dots1[i].className = dots1[i].className.replace(" active");
+}
+slides[slideIndex-1].style.display = "block";
+  
+}
